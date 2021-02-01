f44b0aeeba0c5a98b97df8d616e7128f92942319
yoonsoo <fhkaldh@naver.com>
2021-02-01 20:09:30 +0900

    21.02.01

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+    {
+        id:1000,
+        titile: 'Video_1',
+        description: 'This is Video Number One',
+        views: 24,
+        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        creator: {
+            id: 10000,
+            name: 'Kim',
+            email: 'Kim@gmail.com'
+        }
+    },
+    {
+        id:1001,
+        titile: 'Video_2',
+        description: 'This is Video Number Two',
+        views: 14,
+        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        creator: {
+            id: 10001,
+            name: 'Lee',
+            email: 'Lee@gmail.com'
+        }
+    },
+    {
+        id:1002,
+        titile: 'Video_3',
+        description: 'This is Video Number Three',
+        views: 412,
+        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        creator: {
+            id: 10002,
+            name: 'Park',
+            email: 'Park@gmail.com'
+        }
+    }
+]
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,8 +4,8 @@ import { changePassword, editProfile, userDetail } from '../controllers/userCont
 
 const userRouter = express.Router();
 
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
